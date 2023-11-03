ecbc9b09a229186dab9249ea5746cf8b022b75f9
vikash-frontier <vikash@frontier.xyz>
2023-11-03 14:10:35 +0530

    Made some changes

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
-alert("hii");
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello world from JS!";
+
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+const heading = React.createElement("h1", {}, "Hello world from React");
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "head" }, "Hello h1 tag"),
+    React.createElement("h2", { id: "head2" }, "This is h2")
+  )
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);

5642d9068dfb325d46f4c1888a8751efcc3ac937
vikash-frontier <vikash@frontier.xyz>
2023-11-04 20:16:54 +0530

    Made some changes

--- App.js
+++ App.js
@@ -1,24 +1,72 @@
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello world from JS!";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
-// const root = document.getElementById("root");
-// root.appendChild(heading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <h3>CHWIGGY</h3>
+      </div>
+      <div className="nav-header">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const heading = React.createElement("h1", {}, "Hello world from React");
+const RestaurantCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="card-img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi"
+        alt="meghna-image"
+      />
+      <h3>Meghana food</h3>
+      <h4>Briyani, Fast Food, Asian </h4>
+      <p>4.5 star</p>
+      <p>30 mins</p>
+    </div>
+  );
+};
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "head" }, "Hello h1 tag"),
-    React.createElement("h2", { id: "head2" }, "This is h2")
-  )
-);
+const Body = () => {
+  return (
+    <div className="body-container">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-console.log(heading);
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<App />);

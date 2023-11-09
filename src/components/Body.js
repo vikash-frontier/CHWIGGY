c4c3f67a591c1509c0046fdea96d94611fd97f90
vikash-frontier <vikash@frontier.xyz>
2023-11-09 16:14:28 +0530

    Added the functionality for search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resObj } from "../utils/mockData";
 const Body = () => {
-  const [data, setData] = useState(resObj);
-
-  console.log("hello");
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   const onClickHandle = () => {
-    const filtedData = resObj.filter((item) => item.info.avgRating >= 4.5);
-    setData(filtedData);
+    const filtedData = data.filter((item) => item?.info?.avgRating >= 4.5);
+    setFilteredData(filtedData);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  const getApiData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const res = await data.json();
+
+    // console.log(
+    //   res.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    // );
+    setData(
+      res?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredData(
+      res?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
   return (
     <div className="body-container">
-      <button className="btn" onClick={onClickHandle}>
-        Top Rated Restaurant
-      </button>
+      <div className="filter">
+        <div>
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          {"  "}
+          <button
+            onClick={() => {
+              const filteredSearch = data.filter((item) => {
+                return item.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilteredData(filteredSearch);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        <button className="btn" onClick={onClickHandle}>
+          Top Rated Restaurant
+        </button>
+      </div>
+
       <div className="res-container">
-        {data.map((item) => (
+        {filteredData?.map((item) => (
           <RestaurantCard key={item.info.id} resData={item} />
         ))}
       </div>
